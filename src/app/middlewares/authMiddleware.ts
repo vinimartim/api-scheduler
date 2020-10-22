282342636429c0c7d522e5cc5a5bcb0a9a2a6b79
Vinicius Lisboa <lisboamvinicius@gmail.com>
2020-10-22 13:18:51 -0300

    Add middleware + merge request type from express

---
+++ src/app/middlewares/authMiddleware.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+
+interface TokenPayLoad {
+  id: string
+  iat: number
+  exp: number
+}
+
+export default function authMiddleware(req: Request, res: Response, next: NextFunction) {
+  const { authorization } = req.headers
+
+  if(!authorization) {
+    return res.status(401).json({ message: 'Unauthorized' })
+  }
+
+  const token = authorization.replace('Bearer', '').trim()
+
+  try {
+    const data = jwt.verify(token, 'secret')
+
+    const { id } = data as TokenPayLoad
+
+    req.userId = id
+
+    return next()
+    
+  } catch {
+    return res.status(401).json({ message: 'Unauthorized' })
+  }
+}
