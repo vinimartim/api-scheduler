3032590ef3b3b52fc3c944362b56647334af1b9a
Vinicius Lisboa <lisboamvinicius@gmail.com>
2020-10-22 13:19:08 -0300

    Fix routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,18 @@
 import { Router } from 'express'
+import authMiddleware from './app/middlewares/authMiddleware'
 import AppointmentController from './app/controllers/AppointmentController'
 import UserController from './app/controllers/UserController'
 import AuthController from './app/controllers/AuthController'
 
 const router = Router()
 
+router.post('/auth', AuthController.authenticate)
 router.post('/users', UserController.store)
 
-router.post('/auth', AuthController.authenticate)
-
-router.get('/events', AppointmentController.getAll)
-router.get('/events/:id', AppointmentController.get)
-router.post('/events', AppointmentController.store)
-router.put('/events/:id', AppointmentController.update)
-router.delete('/events/:id', AppointmentController.remove)
-
+router.get('/events', authMiddleware, AppointmentController.getAll)
+router.get('/events/:id', authMiddleware, AppointmentController.get)
+router.post('/events', authMiddleware, AppointmentController.store)
+router.put('/events/:id', authMiddleware, AppointmentController.update)
+router.delete('/events/:id', authMiddleware, AppointmentController.remove)
 
 export default router
